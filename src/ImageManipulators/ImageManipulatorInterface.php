d0945c86dd2d0345a48a184700d748c9344cf590
MatthiasDeWinter <dewinter.matthias@gmail.com>
2015-04-22 08:57:40 +0000

    docblocks

--- src/ImageManipulators/ImageManipulatorInterface.php
+++ src/ImageManipulators/ImageManipulatorInterface.php
@@ -4,6 +4,11 @@ use Spatie\MediaLibrary\Models\Media;
 
 interface ImageManipulatorInterface {
 
+    /**
+     * Create the derived images for given profiles in a model.
+     *
+     * @param Media $media
+     */
     public function createDerivedFilesForMedia(Media $media);
 
 }
--- src/Repositories/MediaLibraryRepositoryInterface.php
+++ src/Repositories/MediaLibraryRepositoryInterface.php
@@ -6,10 +6,33 @@ interface MediaLibraryRepositoryInterface {
 
     public function getCollection(MediaModelInterface $model, $collectionName, $filters);
 
+    /**
+     * Add a new media to a Models mediaCollection
+     *
+     * @param $file
+     * @param MediaModelInterface $model
+     * @param $collectionName
+     * @param bool $preserveOriginal
+     * @param bool $addAsTemporary
+     * @return Media
+     */
     public function add($file, MediaModelInterface $model, $collectionName, $preserveOriginal, $addAsTemporary);
 
+    /**
+     * Remove a media record and it's associated files
+     *
+     * @param $id
+     * @return bool
+     * @throws \Exception
+     */
     public function remove($id);
 
+    /**
+     * Reorder media-records
+     *
+     * @param $orderArray
+     * @param MediaModelInterface $model
+     */
     public function order($orderArray, MediaModelInterface $model);
 
     /**
