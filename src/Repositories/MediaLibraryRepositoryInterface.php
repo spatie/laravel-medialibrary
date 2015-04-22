c375e95b81f6a07433560b33437424125952fe21
freek <freek@spatie.be>
2015-04-22 10:58:14 +0200

    commit

--- src/Repositories/MediaLibraryRepositoryInterface.php
+++ src/Repositories/MediaLibraryRepositoryInterface.php
@@ -4,7 +4,7 @@ use Spatie\MediaLibrary\Models\MediaModelInterface;
 
 interface MediaLibraryRepositoryInterface {
 
-    public function getCollection(MediaModelInterface $model, $collectionName, $filters);
+    public function getCollection(MediaLibraryModelInterface $model, $collectionName, $filters);
 
     public function add($file, MediaLibraryModelInterface $model, $collectionName, $preserveOriginal, $addAsTemporary);
 
