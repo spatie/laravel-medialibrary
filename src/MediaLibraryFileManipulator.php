f1383986788cad32be0dd4115c7767a1cc10b991
freek <freek@spatie.be>
2015-06-28 22:12:29 +0200

    work in progress

--- src/MediaLibraryFileManipulator.php
+++ src/MediaLibraryFileManipulator.php
@@ -2,6 +2,7 @@
 
 namespace Spatie\MediaLibrary;
 
+use File;
 use GlideImage;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Spatie\MediaLibrary\Conversion\ConversionCollectionFactory;
@@ -25,24 +26,41 @@ class MediaLibraryFileManipulator
 
     public function performConversions($conversions, $media)
     {
+        $tempDirectory = storage_path('media-library/temp/' . string()->random(16));
+
+        $copiedOriginalFile = storage_path('media-library/temp/' . string()->random(16) . '.' . pathinfo($media->file), PATHINFO_EXTENSION);
+
+        app(MediaLibraryFileSystem::class)->copyOriginalFile($media, $copiedOriginalFile);
+
         foreach ($conversions as $conversion) {
 
-            $tempFile = storage_path('media-library/temp/' . $media->id . '-' . $conversion->getName() . '.jpg');
+            $conversionResult = $this->performConversion($media, $conversion, $copiedOriginalFile);
 
-            app(MediaLibraryFileSystem::class)->copyOriginalFile($media, $tempFile);
+            app(MediaLibraryFileSystem::class)->copyFileToMediaLibraryForMedia($conversionResult, $media);
 
-            foreach ($conversion->getManipulations() as $manipulation) {
-                (new GlideImage())
-                    ->load($tempFile, $manipulation)
-                    ->useAbsoluteSourceFilePath()
-                    ->save($tempFile);
-            }
-
-            app(MediaLibraryFileSystem::class)->copyFileToMediaLibraryForMedia($tempFile, $media);
-
-            unlink($tempFile);
         }
+
+        File::delete($tempDirectory);
     }
 
+    /**
+     * @param $media
+     * @param $conversion
+     * @param $copiedOriginalFile
+     * @return string
+     */
+    public function performConversion($media, $conversion, $copiedOriginalFile)
+    {
+        $conversionTempFile = storage_path('media-library/temp/' . string()->random(16) . $conversion->getName() . '.' . pathinfo($media->file), PATHINFO_EXTENSION);
 
+        File::copy($copiedOriginalFile, $conversionTempFile);
+
+        foreach ($conversion->getManipulations() as $manipulation) {
+            (new GlideImage())
+                ->load($conversionTempFile, $manipulation)
+                ->useAbsoluteSourceFilePath()
+                ->save($conversionTempFile);
+        }
+        return $conversionTempFile;
+    }
 }
