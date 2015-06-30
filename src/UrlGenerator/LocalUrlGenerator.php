33c62f407d916ea4ee84c2fe390e66829f4625ba
freek <freek@spatie.be>
2015-06-30 19:59:05 +0200

    work in progress

--- src/UrlGenerator/LocalUrlGenerator.php
+++ src/UrlGenerator/LocalUrlGenerator.php
@@ -10,25 +10,34 @@ class LocalUrlGenerator extends BaseUrlGenerator implements UrlGenerator
     {
 
         //nog disk path voor storage path zetten
-        dd(public_path(), $this->config->get('laravel-medialibrary.storage_path'));
 
-        if (!string($this->config->get('laravel-medialibrary.storage_path'))->startsWith(public_path())) {
+        if (! string($this->getStoragePath())->startsWith(public_path())) {
             throw new UrlCouldNotBeDeterminedException('The storage path is not part of the public path');
         }
 
         if ($this->profileName == '') {
-            return $this->getBaseDirectory() . '/' . $this->media->file;
+            return $this->getBaseMediaDirectory() . '/' . $this->media->file_name;
         }
 
-        return $this->getBaseDirectory() . '/' . $this->profileName . '.jpg';
+        return $this->getBaseMediaDirectory() . '/conversions/' . $this->profileName . '.jpg';
     }
 
     /**
      * @return string
      */
-    protected function getBaseDirectory()
+    protected function getBaseMediaDirectory()
     {
-        $baseDirectory = string($this->config->get('laravel-medialibrary.storage_path'))->replace(public_path(), '') . '/media/' . $this->media->id;
+        $baseDirectory = string($this->getStoragePath())->replace(public_path(), '') . '/' . $this->media->id;
+
         return $baseDirectory;
+    }
+
+    protected function getStoragePath()
+    {
+        $diskRootPath = $this->config->get('filesystems.disks.' . $this->config->get('laravel-medialibrary.filesystem') . '.root');
+
+        $configuredPath = $this->config->get('laravel-medialibrary.storage_path');
+
+        return realpath($diskRootPath . '/' . $configuredPath);
     }
 }
