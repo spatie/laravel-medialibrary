c6e689f2c15e88e23f57465577e0cbcb13dd4e59
freek <freek@spatie.be>
2015-07-15 23:58:48 +0200

    add test for regenerate command

---
+++ tests/Commands/RegenerateCommandTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Spatie\MediaLibrary\Test\Conversion;
+
+use Illuminate\Support\Facades\Artisan;
+use File;
+use Spatie\MediaLibrary\Test\TestCase;
+
+class RegenerateCommandTest extends TestCase
+{
+
+    /**
+     * @test
+     */
+    public function it_can_regenerate_all_files()
+    {
+        $media = $this->testModelWithConversion->addMedia($this->getTestFilesDirectory('test.jpg'), 'images');
+
+        $derivedImage = $this->getMediaDirectory("{$media->id}/conversions/thumb.jpg");
+
+        unlink($derivedImage);
+
+        $this->assertFileNotExists($derivedImage);
+
+        Artisan::call('medialibrary:regenerate');
+
+        $this->assertFileExists($derivedImage);
+
+    }
+}
