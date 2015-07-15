da33b584f35fc60926e8bb099f0a177277073642
freek <freek@spatie.be>
2015-07-16 00:45:51 +0200

    add base url generator test

---
+++ tests/UrlGenerator/BaseUrlGeneratorTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Spatie\MediaLibrary\Test\UrlGenerator;
+
+use File;
+use Spatie\MediaLibrary\Conversion\ConversionCollectionFactory;
+use Spatie\MediaLibrary\Test\TestCase;
+use Spatie\MediaLibrary\UrlGenerator\LocalUrlGenerator;
+
+
+class BaseGeneratorTest extends TestCase
+{
+    protected $config;
+
+    /**
+     * @var \Spatie\MediaLibrary\Media
+     */
+    protected $media;
+
+    /**
+     * @var \Spatie\MediaLibrary\Conversion\Conversion
+     */
+    protected $conversion;
+
+    /**
+     * @var LocalUrlGenerator
+     */
+    protected $generator;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->config = app('config');
+
+        $this->media = $this->testModelWithConversion->addMedia($this->getTestFilesDirectory('test.jpg'));
+
+        $this->conversion = ConversionCollectionFactory::createForMedia($this->media)->getByName('thumb');
+
+        // because BaseUrlGenerator is abstract we'll use LocalUrlGenerator to test the methods of base
+        $this->generator = new LocalUrlGenerator($this->config);
+
+        $this->generator
+            ->setMedia($this->media)
+            ->setConversion($this->conversion);
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_get_the_path_relative_to_the_root_of_media_folder()
+    {
+        $pathRelativeToRoot = $this->media->id.'/conversions/'.$this->conversion->getName().'.'.$this->conversion->getResultExtension($this->media->extension);
+
+        $this->assertEquals($pathRelativeToRoot, $this->generator->getPathRelativeToRoot());
+    }
+}
