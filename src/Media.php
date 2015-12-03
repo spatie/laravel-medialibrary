61db743d43c5558b9322ba5318a910b868a04adf
Colin Viebrock <c.viebrock@tippingcanoe.com>
2015-12-02 22:23:29 -0600

    Fixes for scrutinizer

--- src/Media.php
+++ src/Media.php
@@ -128,6 +128,7 @@ class Media extends Model
     /**
      * Determine if the media item has a custom property with the given name.
      *
+     * @param $propertyName
      * @return bool
      */
     public function hasCustomProperty($propertyName)
