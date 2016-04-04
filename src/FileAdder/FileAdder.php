a843006e8bad3c31fb6737befd341c655a178d00
Marc <PhilMarc@users.noreply.github.com>
2016-04-04 17:28:12 +0200

    FileAdder.php sanitizeFileName function
    More sensible sanitize function

--- src/FileAdder/FileAdder.php
+++ src/FileAdder/FileAdder.php
@@ -362,8 +362,20 @@ class FileAdder
      *
      * @return string
      */
-    protected function sanitizeFileName($fileName)
+    protected function sanitizeFileName($fileName, $force_lowercase = true, $anal = false)
     {
-        return str_replace(['#', '/', '\\'], '-', $fileName);
+        $strip = array("~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "=", "+", "[", "{", "]",
+            "}", "\\", "|", ";", ":", "\"", "'", "&#8216;", "&#8217;", "&#8220;", "&#8221;", "&#8211;", "&#8212;",
+            "â€”", "â€“", ",", "<", ".", ">", "/", "?");
+        $clean = trim(str_replace($strip, "", strip_tags($fileName)));
+        $clean = preg_replace('/\s+/', "-", $clean);
+        $clean = ($anal) ? preg_replace("/[^a-zA-Z0-9]/", "", $clean) : $clean ;
+
+        return ($force_lowercase) ?
+        (function_exists('mb_strtolower')) ?
+        mb_strtolower($clean, 'UTF-8') :
+        strtolower($clean) :
+        $clean;
     }
+
 }
