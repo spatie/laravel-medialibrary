bb0e09b17a48d86d249d0eff426b5feccf442bc5
Guillaume Baudoin <gbaudoin@users.noreply.github.com>
2016-04-25 00:17:19 +0200

    Adding SelfHandling declaration for PerformConversions (#207)
    * Adding SelfHandling declaration for PerformConversions
    
    Suggestion for #206
    
    * Namespace for SelfHandling

--- src/Jobs/PerformConversions.php
+++ src/Jobs/PerformConversions.php
@@ -4,12 +4,13 @@ namespace Spatie\MediaLibrary\Jobs;
 
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Bus\SelfHandling;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Spatie\MediaLibrary\Conversion\ConversionCollection;
 use Spatie\MediaLibrary\FileManipulator;
 use Spatie\MediaLibrary\Media;
 
-class PerformConversions extends Job implements ShouldQueue
+class PerformConversions extends Job implements SelfHandling, ShouldQueue
 {
     use InteractsWithQueue, SerializesModels;
 
