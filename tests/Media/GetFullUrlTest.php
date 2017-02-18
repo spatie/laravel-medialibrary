891bb2048a3091db243ed1279d3db89e692ee6f7
Brian Faust <faustbrian@users.noreply.github.com>
2017-02-18 12:37:22 +0200

    Add GetFullUrlTest

---
+++ tests/Media/GetFullUrlTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Spatie\MediaLibrary\Test\Media;
+
+use Spatie\MediaLibrary\Test\TestCase;
+use Spatie\MediaLibrary\Exceptions\InvalidConversion;
+
+class GetFullUrlTest extends TestCase
+{
+    /** @test */
+    public function it_can_get_an_url_of_an_original_item()
+    {
+        $media = $this->testModel->addMedia($this->getTestJpg())->toMediaLibrary();
+
+        $this->assertEquals($media->getFullUrl(), "http://localhost/media/{$media->id}/test.jpg");
+    }
+
+    /** @test */
+    public function it_can_get_an_url_of_a_derived_image()
+    {
+        $media = $this->testModelWithConversion->addMedia($this->getTestJpg())->toMediaLibrary();
+
+        $conversionName = 'thumb';
+
+        $this->assertEquals("http://localhost/media/{$media->id}/conversions/{$conversionName}.jpg", $media->getFullUrl($conversionName));
+    }
+
+    /** @test */
+    public function it_returns_an_exception_when_getting_an_url_for_an_unknown_conversion()
+    {
+        $media = $this->testModel->addMedia($this->getTestJpg())->toMediaLibrary();
+
+        $this->expectException(InvalidConversion::class);
+
+        $media->getFullUrl('unknownConversionName');
+    }
+
+    /** @test */
+    public function it_wil_url_encode_the_file_name_when_generating_an_url()
+    {
+        $this->testModel->addMedia($this->getTestFilesDirectory('test with space.jpg'))->toMediaLibrary();
+
+        $this->assertEquals('http://localhost/media/1/test%20with%20space.jpg', $this->testModel->getFirstMediaUrl());
+    }
+}
