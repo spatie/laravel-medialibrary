04ef765ab73fbcd49781fc72fd11673dcd0e2adb
Sunshine <sunshine.mahant@gmail.com>
2017-03-08 16:04:01 +0530

    Resolved StyleCI issue
    Resolved StyleCI issue with syntax

--- src/MediaLibraryServiceProvider.php
+++ src/MediaLibraryServiceProvider.php
@@ -22,7 +22,7 @@ class MediaLibraryServiceProvider extends ServiceProvider
 
         if (! class_exists('CreateMediaTable')) {
             $this->publishes([
-                __DIR__ . '/../database/migrations/create_media_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_media_table.php'),
+                __DIR__.'/../database/migrations/create_media_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_media_table.php'),
             ], 'migrations');
         }
 
