e32779c595df687ebacc64f8fb22d40e46dc8d24
Sunshine <sunshine.mahant@gmail.com>
2017-03-08 15:53:31 +0530

    code to stop duplicate create_media_table migration
    Added code to check if create_media_table migration exist, if so stop publishing duplicate file.

--- src/MediaLibraryServiceProvider.php
+++ src/MediaLibraryServiceProvider.php
@@ -20,9 +20,11 @@ class MediaLibraryServiceProvider extends ServiceProvider
             __DIR__.'/../config/medialibrary.php' => config_path('medialibrary.php'),
         ], 'config');
 
-        $this->publishes([
-            __DIR__.'/../database/migrations/create_media_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_media_table.php'),
-        ], 'migrations');
+        if (! class_exists('CreateMediaTable')) {
+            $this->publishes([
+                __DIR__ . '/../database/migrations/create_media_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_media_table.php'),
+            ], 'migrations');
+        }
 
         $mediaClass = config('medialibrary.media_model');
 
