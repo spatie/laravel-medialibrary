4463bc51f64c04ecdb25d45b18793a17a750cd05
Roy Duineveld <royduineveld@gmail.com>
2017-07-20 09:47:45 +0200

    Added tests for softdeletes

---
+++ tests/TestModelWithSoftDeletes.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Spatie\MediaLibrary\Test;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\MediaLibrary\HasMedia\HasMediaTrait;
+use Spatie\MediaLibrary\HasMedia\Interfaces\HasMediaConversions;
+
+class TestModelWithSoftDeletes extends Model implements HasMediaConversions
+{
+    use HasMediaTrait, SoftDeletes;
+
+    protected $table = 'test_models';
+    protected $guarded = [];
+    public $timestamps = false;
+
+    public function registerMediaConversions()
+    {
+    }
+}
