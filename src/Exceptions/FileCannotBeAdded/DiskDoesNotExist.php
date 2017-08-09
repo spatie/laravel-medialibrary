12f1121aa564f534b5296901c2893d7b42b28d64
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2017-08-09 08:57:01 +0200

    Removed string typehint from DiskDoesNotExist::create because disk can be null

--- src/Exceptions/FileCannotBeAdded/DiskDoesNotExist.php
+++ src/Exceptions/FileCannotBeAdded/DiskDoesNotExist.php
@@ -6,7 +6,7 @@ use Spatie\MediaLibrary\Exceptions\FileCannotBeAdded;
 
 class DiskDoesNotExist extends FileCannotBeAdded
 {
-    public static function create(string $diskName)
+    public static function create($diskName)
     {
         return new static("There is no filesystem disk named `{$diskName}`");
     }
