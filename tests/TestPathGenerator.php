09c853c198756eb9850d751041397b1c7757e802
Brent Roose <brendt@stitcher.io>
2017-10-19 09:09:22 +0000

    Apply fixes from StyleCI

--- tests/TestPathGenerator.php
+++ tests/TestPathGenerator.php
@@ -19,6 +19,6 @@ class TestPathGenerator implements PathGenerator
 
     public function getPathForConversions(Media $media): string
     {
-        return $this->getPath($media) . '/convertions/';
+        return $this->getPath($media).'/convertions/';
     }
 }
