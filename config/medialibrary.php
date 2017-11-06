8b5e272199eeea5a18dc186d62ee66312a73aa05
Jürgen van Dijk <juukie@users.noreply.github.com>
2017-11-07 00:00:49 +0100

    Remove trailing space

--- config/medialibrary.php
+++ config/medialibrary.php
@@ -9,7 +9,7 @@ return [
     'default_filesystem' => 'public',
 
     /*
-     * The maximum file size of an item in bytes. 
+     * The maximum file size of an item in bytes.
      * Adding a larger file will result in an exception.
      */
     'max_file_size' => 1024 * 1024 * 10,
