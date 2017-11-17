f76d71d6b644dcfd02e91e5c4f213e8632bd7efc
Brent Roose <brendt@stitcher.io>
2017-11-17 07:50:12 +0000

    Apply fixes from StyleCI

--- src/Conversion/ConversionCollection.php
+++ src/Conversion/ConversionCollection.php
@@ -125,7 +125,7 @@ class ConversionCollection extends Collection
     /*
      * Get all the conversions in the collection that should be queued.
      */
-    public function getQueuedConversions(string $collectionName = ''): ConversionCollection
+    public function getQueuedConversions(string $collectionName = ''): self
     {
         return $this->getConversions($collectionName)->filter->shouldBeQueued();
     }
@@ -143,7 +143,7 @@ class ConversionCollection extends Collection
     /*
      * Get all the conversions in the collection that should not be queued.
      */
-    public function getNonQueuedConversions(string $collectionName = ''): ConversionCollection
+    public function getNonQueuedConversions(string $collectionName = ''): self
     {
         return $this->getConversions($collectionName)->reject->shouldBeQueued();
     }
@@ -151,7 +151,7 @@ class ConversionCollection extends Collection
     /**
      * Return the list of conversion files.
      */
-    public function getConversionsFiles(string $collectionName = ''): ConversionCollection
+    public function getConversionsFiles(string $collectionName = ''): self
     {
         return $this->getConversions($collectionName)->map(function (Conversion $conversion) {
             return "{$conversion->getName()}.{$conversion->getResultExtension()}";
