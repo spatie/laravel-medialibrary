18b12a8cd933b86e7210c2d5881f09d07175a385
Freek Van der Herten <freek@spatie.be>
2017-11-21 16:55:28 +0000

    Apply fixes from StyleCI

--- tests/HasMediaConversionsTrait/MediaCollectionTest.php
+++ tests/HasMediaConversionsTrait/MediaCollectionTest.php
@@ -11,11 +11,9 @@ class MediaCollectionTest extends TestCase
     /** @test */
     public function it_will_use_the_disk_from_a_media_collection()
     {
-        $testModel = new class extends TestModelWithConversion
-        {
+        $testModel = new class extends TestModelWithConversion {
             public function registerMediaConversions(Media $media = null)
             {
-
             }
 
             public function registerMediaCollections()
@@ -41,11 +39,9 @@ class MediaCollectionTest extends TestCase
     /** @test */
     public function it_will_not_use_the_disk_name_of_the_collection_if_a_diskname_is_specified_while_adding()
     {
-        $testModel = new class extends TestModelWithConversion
-        {
+        $testModel = new class extends TestModelWithConversion {
             public function registerMediaConversions(Media $media = null)
             {
-
             }
 
             public function registerMediaCollections()
