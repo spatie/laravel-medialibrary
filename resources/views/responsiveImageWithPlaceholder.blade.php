e6fbef2d88a39e3d336659abf144609fe48ad1b7
freek <freek@spatie.be>
2017-12-08 15:43:22 +0100

    commit

--- resources/views/responsiveImageWithPlaceholder.blade.php
+++ resources/views/responsiveImageWithPlaceholder.blade.php
@@ -1,1 +1,2 @@
-<img{!! $attributeString !!} srcset="{{ $media->getSrcset($conversion) }}" onload="if(this.dataset.sized===undefined){this.sizes=Math.ceil(this.getBoundingClientRect().width)+'px';this.dataset.sized=''}" sizes="1px" src="{{ $media->getUrl($conversion) }}" width="{{ $width }}">
+<img{!! $attributeString !!} srcset="{{ $media->getSrcset($conversion) }}" onload="if(this.dataset.sized===undefined){this.sizes=Math.ceil(this.getBoundingClientRect().width)+'px';this.dataset.sized=''}"
+ sizes="1px" src="{{ $media->getUrl($conversion) }}" width="{{ $width }}">
