04a6b61e72fcfecc322bf225fa42244e7206df1c
freek <freek@spatie.be>
2017-12-08 12:58:40 +0100

    add test

--- tests/ZipStreamResponseTest.php
+++ tests/ZipStreamResponseTest.php
@@ -1,2 +1,36 @@
 <?php
 
+namespace Spatie\MediaLibrary\Tests;
+
+use Spatie\MediaLibrary\Media;
+use Illuminate\Support\Facades\Route;
+use Spatie\MediaLibrary\Tests\TestCase;
+use Spatie\MediaLibrary\ZipStreamResponse;
+use Symfony\Component\HttpFoundation\StreamedResponse;
+
+class ZipStreamResponseTest extends TestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+
+        foreach (range(1, 3) as $i) {
+            $this->testModel
+                ->addMedia($this->getTestJpg())
+                ->preservingOriginal()
+                ->toMediaCollection();
+        }
+    }
+
+    /** @test */
+    public function it_can_return_a_stream_of_media()
+    {
+        Route::get('stream-test', function () {
+            return ZipStreamResponse::create('my-media.zip')->addMedia(Media::all());
+        });
+
+        $response = $this->get('stream-test');
+
+        $this->assertInstanceOf(StreamedResponse::class, $response->baseResponse);
+    }
+}
