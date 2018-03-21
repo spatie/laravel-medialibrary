ecec26548403bbf2f7a7eaa2de54181881efa2d5
Ishan Demon <ishandemon@gmail.com>
2018-03-21 17:44:41 +0530

    Fixing bug #970
     addMediaFromUrl fail when url is little bit weird.

--- src/HasMedia/HasMediaTrait.php
+++ src/HasMedia/HasMediaTrait.php
@@ -129,7 +129,12 @@ trait HasMediaTrait
         $this->guardAgainstInvalidMimeType($tmpFile, $allowedMimeTypes);
 
         $filename = basename(parse_url($url, PHP_URL_PATH));
-
+        
+        if($filename == ''){
+            $mediaExt = explode('/',mime_content_type($tmpFile));
+            $filename = 'file.'.$mediaExt[1];
+        }
+        
         return app(FileAdderFactory::class)
             ->create($this, $tmpFile)
             ->usingName(pathinfo($filename, PATHINFO_FILENAME))
