cd25ca9365b6be138f21a6c2454c321202b85f92
Brent Roose <brent@spatie.be>
2018-03-21 13:35:35 +0100

    Add failing test

--- tests/Feature/ResponsiveImages/RegisteredResponsiveImagesTest.php
+++ tests/Feature/ResponsiveImages/RegisteredResponsiveImagesTest.php
@@ -2,6 +2,8 @@
 
 namespace Spatie\MediaLibrary\Tests\Feature\ResponsiveImages;
 
+use Spatie\MediaLibrary\Models\Media;
+use Spatie\MediaLibrary\ResponsiveImages\RegisteredResponsiveImages;
 use Spatie\MediaLibrary\Tests\TestCase;
 
 class RegisteredResponsiveImagesTest extends TestCase
@@ -21,5 +23,28 @@ class RegisteredResponsiveImagesTest extends TestCase
             'test___medialibrary_original_284_233.jpg',
             'test___medialibrary_original_237_195.jpg',
         ], $media->responsive_images['medialibrary_original']['urls']);
+    }
+
+    /** @test */
+    public function it_can_render_a_srcset_when_the_base64svg_is_not_rendered_yet()
+    {
+        $this->testModel
+            ->addMedia($this->getTestJpg())
+            ->withResponsiveImages()
+            ->toMediaCollection();
+
+        $media = $this->testModel->getFirstMedia();
+
+        $responsiveImages = $media->responsive_images;
+
+        unset($responsiveImages['medialibrary_original']['base64svg']);
+
+        $media->responsive_images = $responsiveImages;
+
+        $registeredResponsiveImage = new RegisteredResponsiveImages($media);
+
+        $this->assertNull($registeredResponsiveImage->getPlaceholderSvg());
+
+        $this->assertNotEmpty($registeredResponsiveImage->getSrcset());
     }
 }
