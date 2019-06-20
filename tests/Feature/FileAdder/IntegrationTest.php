0edaa88248d728f14911a6c6f0fb9e41080e046b
Rias <hello@rias.be>
2019-06-20 11:05:02 +0200

    Fix image url - fix #1472

--- tests/Feature/FileAdder/IntegrationTest.php
+++ tests/Feature/FileAdder/IntegrationTest.php
@@ -285,7 +285,7 @@ class IntegrationTest extends TestCase
     /** @test */
     public function it_can_add_a_remote_file_to_the_medialibrary()
     {
-        $url = 'https://docs.spatie.be/images/medialibrary/header.jpg';
+        $url = 'https://docs.spatie.be/laravel-medialibrary/v7/images/header.jpg';
 
         $media = $this->testModel
             ->addMediaFromUrl($url)
@@ -346,7 +346,7 @@ class IntegrationTest extends TestCase
     /** @test */
     public function it_wil_throw_an_exception_when_a_remote_file_has_an_invalid_mime_type()
     {
-        $url = 'https://docs.spatie.be/images/medialibrary/header.jpg';
+        $url = 'https://docs.spatie.be/laravel-medialibrary/v7/images/header.jpg';
 
         $this->expectException(MimeTypeNotAllowed::class);
 
