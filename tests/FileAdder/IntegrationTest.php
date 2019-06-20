fa4ee596875bc7dd9548d49511f2fc58d5e5726d
Rias <hello@rias.be>
2019-06-20 11:03:31 +0200

    Fix v5 test

--- tests/FileAdder/IntegrationTest.php
+++ tests/FileAdder/IntegrationTest.php
@@ -295,7 +295,7 @@ class IntegrationTest extends TestCase
     /** @test */
     public function it_can_add_a_remote_file_to_the_medialibrary()
     {
-        $url = 'https://docs.spatie.be/images/medialibrary/header.jpg';
+        $url = 'https://docs.spatie.be/laravel-medialibrary/v5/images/header.jpg';
 
         $media = $this->testModel
             ->addMediaFromUrl($url)
@@ -320,7 +320,7 @@ class IntegrationTest extends TestCase
     /** @test */
     public function it_wil_throw_an_exception_when_a_remote_file_has_an_invalid_mime_type()
     {
-        $url = 'https://docs.spatie.be/images/medialibrary/header.jpg';
+        $url = 'https://docs.spatie.be/laravel-medialibrary/v5/images/header.jpg';
 
         $this->expectException(MimeTypeNotAllowed::class);
 
