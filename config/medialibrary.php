c105b2b4a2ada6b9711e9ad9aa739ba75b647f56
Christoph Van Hees <christoph.vanhees@hotmail.com>
2019-07-11 16:01:39 +0200

    Add environment variable to change disk (#1424)
    Ability to switch disk on different servers or environments.

--- config/medialibrary.php
+++ config/medialibrary.php
@@ -6,7 +6,7 @@ return [
      * The disk on which to store added files and derived images by default. Choose
      * one or more of the disks you've configured in config/filesystems.php.
      */
-    'disk_name' => 'public',
+    'disk_name' => env('MEDIA_DISK', 'public'),
 
     /*
      * The maximum file size of an item in bytes.
