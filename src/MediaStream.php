370158266c3ae2ca1e8bfcbf63b62511d26deee9
Ruben Van Assche <ruben@spatie.be>
2019-09-25 08:47:37 +0200

    Fix media stream not working (#1571)
    * wip
    
    * Update MediaStream.php

--- src/MediaStream.php
+++ src/MediaStream.php
@@ -60,19 +60,33 @@ class MediaStream implements Responsable
 
     public function toResponse($request)
     {
+        $headers = [
+            'Content-Disposition' => "attachment; filename=\"{$this->zipName}\"",
+            'Content-Type' => 'application/octet-stream',
+        ];
+
         return new StreamedResponse(function () {
-            $zip = new ZipStream($this->zipName);
+            return $this->getZipStream();
+        }, 200, $headers);
+    }
 
-            $this->getZipStreamContents()->each(function (array $mediaInZip) use ($zip) {
-                $stream = $mediaInZip['media']->stream();
+    public function getZipStream(): ZipStream
+    {
+        $zip = new ZipStream($this->zipName);
 
-                $zip->addFileFromStream($mediaInZip['fileNameInZip'], $stream);
+        $this->getZipStreamContents()->each(function (array $mediaInZip) use ($zip) {
+            $stream = $mediaInZip['media']->stream();
 
+            $zip->addFileFromStream($mediaInZip['fileNameInZip'], $stream);
+
+            if (is_resource($stream)) {
                 fclose($stream);
-            });
-
-            $zip->finish();
+            }
         });
+
+        $zip->finish();
+
+        return $zip;
     }
 
     protected function getZipStreamContents(): Collection
