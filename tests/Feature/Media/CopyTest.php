b1ec9a3e80583f221dba769941257f5cee69923f
Daniel Verner <daniel.verner@42coders.com>
2019-10-07 12:29:32 +0200

    Fixes #1258 when the file doesn't have extension
    The temporary directory cannot identify if it is a file path, when the file doesn't have extension, and it creates a directory with the file's name.

--- tests/Feature/Media/CopyTest.php
+++ tests/Feature/Media/CopyTest.php
@@ -37,4 +37,41 @@ class CopyTest extends TestCase
         $this->assertEquals($movedMedia->name, 'custom-name');
         $this->assertEquals($movedMedia->getCustomProperty('custom-property-name'), 'custom-property-value');
     }
+
+    /** @test */
+    public function it_can_copy_file_without_extension()
+    {
+        if(!file_exists(storage_path('medialibrary/temp'))){
+            mkdir(storage_path('medialibrary/temp'), 0777, true);
+        }
+
+        config(['medialibrary.temporary_directory_path' => realpath(storage_path('medialibrary/temp'))]);
+
+        /** @var TestModel $model */
+        $model = TestModel::create(['name' => 'test']);
+
+        /** @var \Spatie\MediaLibrary\Models\Media $media */
+        $media = $model
+            ->addMedia($this->getTestImageWithoutExtension())
+            ->usingName('custom-name')
+            ->withCustomProperties(['custom-property-name' => 'custom-property-value'])
+            ->toMediaCollection();
+
+        $this->assertFileExists($this->getMediaDirectory($media->id.'/image'));
+
+        $anotherModel = TestModel::create(['name' => 'another-test']);
+
+        $movedMedia = $media->copy($anotherModel, 'images');
+
+        $movedMedia->refresh();
+
+        $this->assertCount(1, $model->getMedia('default'));
+        $this->assertFileExists($this->getMediaDirectory($media->id.'/image'));
+
+        $this->assertCount(1, $anotherModel->getMedia('images'));
+        $this->assertFileExists($this->getMediaDirectory($movedMedia->id.'/image'));
+        $this->assertEquals($movedMedia->model->id, $anotherModel->id);
+        $this->assertEquals($movedMedia->name, 'custom-name');
+        $this->assertEquals($movedMedia->getCustomProperty('custom-property-name'), 'custom-property-value');
+    }
 }
