30d83a02675f5fbba3d22fd1a6ff00d27d65521b
Musa <40173603+voyula@users.noreply.github.com>
2020-01-18 14:18:39 +0300

    Update HasMedia.php

--- src/HasMedia/HasMedia.php
+++ src/HasMedia/HasMedia.php
@@ -18,10 +18,10 @@ use Spatie\MediaLibrary\Exceptions\MediaCannotBeDeleted;
 use Spatie\MediaLibrary\Exceptions\MediaCannotBeUpdated;
 use Spatie\MediaLibrary\FileAdder\FileAdder;
 use Spatie\MediaLibrary\FileAdder\FileAdderFactory;
+use Spatie\MediaLibrary\HasMedia\Contracts\HasMedia;
 use Spatie\MediaLibrary\MediaCollection\MediaCollection;
 use Spatie\MediaLibrary\MediaRepository;
 use Spatie\MediaLibrary\Models\Media;
-use Spatie\MediaLibrary\HasMedia\Contracts\HasMedia;
 
 trait HasMedia
 {
