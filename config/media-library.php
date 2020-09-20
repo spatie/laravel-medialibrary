a9fac77f12e9a00567509327939d04359eeff3cd
Chrysanthos Prodromou <chrysanthos.prodromou@gmail.com>
2020-09-20 12:45:56 +0300

    Allow to hook into how the images are downloaded

--- config/media-library.php
+++ config/media-library.php
@@ -92,6 +92,13 @@ return [
     'url_generator' => Spatie\MediaLibrary\Support\UrlGenerator\DefaultUrlGenerator::class,
 
     /*
+     * When using the addMediaFromUrl method you may want to replace the default downloader.
+     * This is particularly useful when the url of the image is behind a firewall and
+     * need to add additional flags, possibly using curl.
+     */
+    'media_downloader' => Spatie\MediaLibrary\Downloaders\DefaultDownloader::class,
+
+    /*
      * Whether to activate versioning when urls to files get generated.
      * When activated, this attaches a ?v=xx query string to the URL.
      */
---
+++ src/Downloaders/DefaultDownloader.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Spatie\MediaLibrary\Downloaders;
+
+use Spatie\MediaLibrary\MediaCollections\Exceptions\UnreachableUrl;
+
+class DefaultDownloader
+{
+    public function getTempFile($url)
+    {
+        if (!$stream = @fopen($url, 'r')) {
+            throw UnreachableUrl::create($url);
+        }
+
+        $temporaryFile = tempnam(sys_get_temp_dir(), 'media-library');
+        file_put_contents($temporaryFile, $stream);
+
+        return $temporaryFile;
+    }
+}
