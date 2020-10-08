bab40ba8327e6694a9f85c5c26f7a627c9277d2d
freek <freek@spatie.be>
2020-10-08 10:24:39 +0200

    wip

--- src/Conversions/Actions/PerformConversionAction.php
+++ src/Conversions/Actions/PerformConversionAction.php
@@ -5,8 +5,10 @@ namespace Spatie\MediaLibrary\Conversions\Actions;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Str;
 use Spatie\MediaLibrary\Conversions\Conversion;
+use Spatie\MediaLibrary\Conversions\Events\ConversionHasBeenCompleted;
 use Spatie\MediaLibrary\Conversions\Events\ConversionWillStart;
 use Spatie\MediaLibrary\Conversions\ImageGenerators\ImageGenerator;
+use Spatie\MediaLibrary\MediaCollections\Filesystem;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
 use Spatie\MediaLibrary\ResponsiveImages\ResponsiveImageGenerator;
 use Spatie\MediaLibrary\Support\ImageFactory;
@@ -23,7 +25,7 @@ class PerformConversionAction
 
         $copiedOriginalFile = $imageGenerator->convert($copiedOriginalFile, $conversion);
 
-        $manipulationResult = $this->performManipulations($media, $conversion, $copiedOriginalFile);
+        $manipulationResult = (new PerformManipulationsAction)->execute($media, $conversion, $copiedOriginalFile);
 
         $newFileName = $conversion->getConversionFile($media);
 
@@ -39,43 +41,12 @@ class PerformConversionAction
                 $renamedFile
             );
         }
-    }
 
-    protected function performManipulations(
-        Media $media,
-        Conversion $conversion,
-        string $imageFile
-    ): string {
-        if ($conversion->getManipulations()->isEmpty()) {
-            return $imageFile;
-        }
+        app(Filesystem::class)->copyToMediaLibrary($renamedFile, $media, 'conversions');
 
-        $conversionTempFile = $this->getConversionTempFileName($media, $conversion, $imageFile);
+        $media->markAsConversionGenerated($conversion->getName());
 
-        File::copy($imageFile, $conversionTempFile);
-
-        $supportedFormats = ['jpg', 'pjpg', 'png', 'gif'];
-        if ($conversion->shouldKeepOriginalImageFormat() && in_array($media->extension, $supportedFormats)) {
-            $conversion->format($media->extension);
-        }
-
-        ImageFactory::load($conversionTempFile)
-            ->manipulate($conversion->getManipulations())
-            ->save();
-
-        return $conversionTempFile;
-    }
-
-    protected function getConversionTempFileName(
-        Media $media,
-        Conversion $conversion,
-        string $imageFile
-    ): string {
-        $directory = pathinfo($imageFile, PATHINFO_DIRNAME);
-
-        $fileName = Str::random(32)."{$conversion->getName()}.{$media->extension}";
-
-        return "{$directory}/{$fileName}";
+        event(new ConversionHasBeenCompleted($media, $conversion));
     }
 
     protected function renameInLocalDirectory(
