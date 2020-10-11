9ea27a5a449f6fbe4ee463ec8b0d30cfc24b75ec
rcerljenko <rcerljenko@users.noreply.github.com>
2020-10-11 15:58:59 +0000

    Fix styling

--- src/ResponsiveImages/WidthCalculator/FixedWidthCalculator.php
+++ src/ResponsiveImages/WidthCalculator/FixedWidthCalculator.php
@@ -6,12 +6,12 @@ use Illuminate\Support\Collection;
 
 class FixedWidthCalculator extends FileSizeOptimizedWidthCalculator
 {
-	public function calculateWidths(int $fileSize, int $width, int $height): Collection
-	{
-		$breakpoints = config('media-library.responsive_images.breakpoints');
+    public function calculateWidths(int $fileSize, int $width, int $height): Collection
+    {
+        $breakpoints = config('media-library.responsive_images.breakpoints');
 
-		return empty($breakpoints) ? parent::calculateWidths($fileSize, $width, $height) : collect($breakpoints)->filter(function ($value, $key) use ($width) {
-			return $value < $width;
-		});
-	}
+        return empty($breakpoints) ? parent::calculateWidths($fileSize, $width, $height) : collect($breakpoints)->filter(function ($value, $key) use ($width) {
+            return $value < $width;
+        });
+    }
 }
