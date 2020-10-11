f7f9204100ebac260806ad28232e7d43d4c9fe1c
Ricardo Čerljenko <ricardo.cerljenko@gmail.com>
2020-10-11 17:58:12 +0200

    added fixed width calculator logic

---
+++ src/ResponsiveImages/WidthCalculator/FixedWidthCalculator.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Spatie\MediaLibrary\ResponsiveImages\WidthCalculator;
+
+use Illuminate\Support\Collection;
+
+class FixedWidthCalculator extends FileSizeOptimizedWidthCalculator
+{
+	public function calculateWidths(int $fileSize, int $width, int $height): Collection
+	{
+		$breakpoints = config('media-library.responsive_images.breakpoints');
+
+		return empty($breakpoints) ? parent::calculateWidths($fileSize, $width, $height) : collect($breakpoints)->filter(function ($value, $key) use ($width) {
+			return $value < $width;
+		});
+	}
+}
