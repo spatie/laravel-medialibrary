aeea3f2a1af86895f7f57ae5dea74e0676aaea51
Nielsvanpach <Nielsvanpach@users.noreply.github.com>
2020-10-12 09:26:49 +0000

    Fix styling

--- tests/TestSupport/TestFileNamer.php
+++ tests/TestSupport/TestFileNamer.php
@@ -6,7 +6,6 @@ use Spatie\MediaLibrary\Support\FileNamer\FileNamer;
 
 class TestFileNamer extends FileNamer
 {
-
     public function getFileName(string $fileName): string
     {
         return 'testing_file_namer';
