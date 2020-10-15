74c332372bdeb747120595f9f169e261299242e1
Niels <niels@spatie.be>
2020-10-15 11:15:43 +0200

    apply code review suggestions

--- tests/ResponsiveImages/ResponsiveImageFileNamerTest.php
+++ tests/ResponsiveImages/ResponsiveImageFileNamerTest.php
@@ -6,14 +6,10 @@ use Spatie\MediaLibrary\Tests\TestSupport\TestFileNamer;
 
 class ResponsiveImageFileNamerTest extends ResponsiveImageTest
 {
-    /**
-     * Runs the same set of tests as ResponsiveImageTest, but with a different
-     * File namer.
-     */
     public function setUp(): void
     {
         parent::setUp();
-        \Config::set("media-library.file_namer", TestFileNamer::class);
+        config()->set("media-library.file_namer", TestFileNamer::class);
         $this->file_name = "testing_file_namer";
     }
 }
