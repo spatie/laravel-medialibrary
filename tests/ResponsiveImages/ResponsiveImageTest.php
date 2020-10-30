c647fc6f19c5c7e02b093369e4ef92674d778b6d
Niels <niels@spatie.be>
2020-10-30 09:52:54 +0100

    fix test case

--- tests/ResponsiveImages/ResponsiveImageTest.php
+++ tests/ResponsiveImages/ResponsiveImageTest.php
@@ -7,6 +7,7 @@ use Spatie\MediaLibrary\Tests\TestCase;
 class ResponsiveImageTest extends TestCase
 {
     public string $fileName = 'test';
+    public string $fileNameWithUnderscore = 'test_';
 
     /** @test */
     public function a_media_instance_can_get_responsive_image_urls()
@@ -107,8 +108,6 @@ class ResponsiveImageTest extends TestCase
     /** @test  */
     public function it_can_handle_file_names_with_underscore()
     {
-        $fileName = 'test_';
-
         $this
             ->testModelWithResponsiveImages
             ->addMedia($this->getTestImageEndingWithUnderscore())
@@ -118,13 +117,13 @@ class ResponsiveImageTest extends TestCase
         $media = $this->testModelWithResponsiveImages->getFirstMedia();
 
         $this->assertSame([
-            "http://localhost/media/1/responsive-images/{$fileName}___media_library_original_340_280.jpg",
-            "http://localhost/media/1/responsive-images/{$fileName}___media_library_original_284_233.jpg",
-            "http://localhost/media/1/responsive-images/{$fileName}___media_library_original_237_195.jpg",
+            "http://localhost/media/1/responsive-images/{$this->fileNameWithUnderscore}___media_library_original_340_280.jpg",
+            "http://localhost/media/1/responsive-images/{$this->fileNameWithUnderscore}___media_library_original_284_233.jpg",
+            "http://localhost/media/1/responsive-images/{$this->fileNameWithUnderscore}___media_library_original_237_195.jpg",
         ], $media->getResponsiveImageUrls());
 
         $this->assertSame([
-            "http://localhost/media/1/responsive-images/{$fileName}___thumb_50_41.jpg",
+            "http://localhost/media/1/responsive-images/{$this->fileNameWithUnderscore}___thumb_50_41.jpg",
         ], $media->getResponsiveImageUrls("thumb"));
 
         $this->assertSame([], $media->getResponsiveImageUrls("non-existing-conversion"));
