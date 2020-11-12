b407f5aa15f98e0701e31e38fed61eaae1a0ba1a
Jon Geerars <51792370+Jon78@users.noreply.github.com>
2020-11-12 13:59:17 +0100

    Update media-library.php (#2171)
    This PR streamlines the default config with the config in spatie/laravel-image-optimizer, resulting in better compression from jpegs and happier people all around

--- config/media-library.php
+++ config/media-library.php
@@ -66,6 +66,7 @@ return [
      */
     'image_optimizers' => [
         Spatie\ImageOptimizer\Optimizers\Jpegoptim::class => [
+            '-m85', // set maximum quality to 85%
             '--strip-all', // this strips out all text information such as comments and EXIF data
             '--all-progressive', // this will make sure the resulting image is a progressive one
         ],
@@ -83,6 +84,12 @@ return [
         Spatie\ImageOptimizer\Optimizers\Gifsicle::class => [
             '-b', // required parameter for this package
             '-O3', // this produces the slowest but best results
+        ],
+        Spatie\ImageOptimizer\Optimizers\Cwebp::class => [
+                '-m 6', // for the slowest compression method in order to get the best compression.
+                '-pass 10', // for maximizing the amount of analysis pass.
+                '-mt', // multithreading for some speed improvements.
+                '-q 90', //quality factor that brings the least noticeable changes.
         ],
     ],
 
