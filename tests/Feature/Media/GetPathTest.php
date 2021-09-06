8e14fd39e2a37c92119c3e3c79304e1ab8edbff0
Kevin Purwito <kevin.purwito@gmail.com>
2021-09-06 11:38:12 +0700

    Add tests for prefix feature in GetPathTest.php

--- tests/Feature/Media/GetPathTest.php
+++ tests/Feature/Media/GetPathTest.php
@@ -12,7 +12,18 @@ class GetPathTest extends TestCase
     {
         $media = $this->testModel->addMedia($this->getTestJpg())->toMediaCollection();
 
-        $this->assertEquals($media->getPath(), $this->getMediaDirectory()."/{$media->id}/test.jpg");
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+            $expected = str_replace('/', '\\', $this->getMediaDirectory() . "/{$media->id}/test.jpg");
+            $actual = str_replace('/', '\\', $media->getPath());
+
+            $this->assertEquals($expected, $actual);
+
+            return 'windows-os-test';
+        }
+
+        $this->assertEquals($this->getMediaDirectory() . "/{$media->id}/test.jpg", $media->getPath());
+
+        return 'linux-os-test';
     }
 
     /** @test */
@@ -21,11 +32,21 @@ class GetPathTest extends TestCase
         $media = $this->testModelWithConversion->addMedia($this->getTestJpg())->toMediaCollection();
 
         $conversionName = 'thumb';
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+            $expected = str_replace('/', '\\', $this->getMediaDirectory() . "/{$media->id}/conversions/test-{$conversionName}.jpg");
+            $actual = str_replace('/', '\\', $media->getPath($conversionName));
+
+            $this->assertEquals($expected, $actual);
+
+            return 'windows-os-test';
+        }
 
         $this->assertEquals(
-            $this->getMediaDirectory()."/{$media->id}/conversions/test-{$conversionName}.jpg",
+            $this->getMediaDirectory() . "/{$media->id}/conversions/test-{$conversionName}.jpg",
             $media->getPath($conversionName)
         );
+
+        return 'linux-os-test';
     }
 
     /** @test */
@@ -37,4 +58,52 @@ class GetPathTest extends TestCase
 
         $media->getPath('unknownConversionName');
     }
+
+    /** @test */
+    public function it_can_get_a_path_of_an_original_item_with_prefix()
+    {
+        config(['media-library.prefix' => 'prefix']);
+
+        $media = $this->testModel->addMedia($this->getTestJpg())->toMediaCollection();
+
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+            $expected = str_replace('/', '\\', $this->getMediaDirectory() . "/prefix/{$media->id}/test.jpg");
+            $actual = str_replace('/', '\\', $media->getPath());
+
+            $this->assertEquals($expected, $actual);
+
+            return 'windows-os-test';
+        }
+
+        $this->assertEquals($this->getMediaDirectory() . "/prefix/{$media->id}/test.jpg", $media->getPath());
+
+        return 'linux-os-test';
+    }
+
+    /** @test */
+    public function it_can_get_a_path_of_a_derived_image_with_prefix()
+    {
+        config(['media-library.prefix' => 'prefix']);
+
+        $media = $this->testModelWithConversion->addMedia($this->getTestJpg())->toMediaCollection();
+
+        $conversionName = 'thumb';
+
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+            $expected = str_replace('/', '\\', $this->getMediaDirectory() . "/prefix/{$media->id}/conversions/test-{$conversionName}.jpg");
+            $actual = str_replace('/', '\\', $media->getPath($conversionName));
+
+            $this->assertEquals($expected, $actual);
+
+            return 'windows-os-test';
+        }
+
+        $this->assertEquals(
+            $this->getMediaDirectory() . "/prefix/{$media->id}/conversions/test-{$conversionName}.jpg",
+            $media->getPath($conversionName)
+        );
+
+        return 'linux-os-test';
+    }
+
 }
