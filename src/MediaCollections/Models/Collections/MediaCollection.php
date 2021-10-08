9b8d7998b2860926f53ed3a12acffc57c1050743
freekmurze <freekmurze@users.noreply.github.com>
2021-10-08 06:35:06 +0000

    Fix styling

--- src/MediaCollections/Models/Collections/MediaCollection.php
+++ src/MediaCollections/Models/Collections/MediaCollection.php
@@ -30,7 +30,7 @@ class MediaCollection extends Collection implements Htmlable
     {
         return $this->sum('size');
     }
-    
+
     public function toArray()
     {
         if (! ($this->formFieldName ?? $this->collectionName)) {
