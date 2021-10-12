601097328a703dfe8da14186148b29d3c77fa060
Dustin Boling <dustin@dustinboling.com>
2021-10-12 16:13:52 -0700

    Adds toArray method for Inertia.js support
    The Inertia::render method calls the toArray method on the MediaCollection. Without this toArray method, the Media objects were being returned without image preview URLs. Adding the toArray method fixes this issue.

--- src/MediaCollections/Models/Collections/MediaCollection.php
+++ src/MediaCollections/Models/Collections/MediaCollection.php
@@ -30,6 +30,16 @@ class MediaCollection extends Collection implements Htmlable
     {
         return $this->sum('size');
     }
+    
+    public function toArray()
+    {
+        return $this->map(function (Media $media) {
+            return array_merge($media->toArray(), [
+                'preview_url' => $media->hasGeneratedConversion('preview') ? $media->getUrl('preview') : null,
+                'original_url' => $media->getUrl()
+            ]);
+        })->all();
+    }
 
     public function toHtml()
     {
