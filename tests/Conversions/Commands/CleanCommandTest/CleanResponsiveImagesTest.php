a99b27a48b7615cdaf839c2ece566978eabf46aa
Michael Lindahl <me@michaellindahl.com>
2022-08-19 10:40:14 -0700

    Add unit test for active conversions without responsive images

--- tests/Conversions/Commands/CleanCommandTest/CleanResponsiveImagesTest.php
+++ tests/Conversions/Commands/CleanCommandTest/CleanResponsiveImagesTest.php
@@ -1,8 +1,6 @@
 <?php
 
-
-
-it('can clean responsive images', function () {
+it('can clean responsive images for deprecated conversions', function () {
     $media = $this->testModelWithResponsiveImages
         ->addMedia($this->getTestJpg())
         ->preservingOriginal()
@@ -25,4 +23,30 @@ it('can clean responsive images', function () {
 
     expect($media->responsive_images)->toEqual($originalResponsiveImagesContent);
     $this->assertFileDoesNotExist($deprecatedReponsiveImagesPath);
+});
+
+it('can clean responsive images for active conversions without responsive images', function () {
+    $media = $this->testModelWithConversion
+            ->addMedia($this->getTestJpg())
+            ->preservingOriginal()
+            ->toMediaCollection();
+    
+    $thumbResponsiveImageFileName = "{$media->file_name}___thumb_340_280.jpg";
+    $thumbReponsiveImagesPath = $this->getMediaDirectory("{$media->id}/responsive-images/{$thumbResponsiveImageFileName}");
+    mkdir($this->getMediaDirectory("{$media->id}/responsive-images"));
+    touch($thumbReponsiveImagesPath);
+
+    $originalResponsiveImagesContent = $media->responsive_images;
+    $newResponsiveImages = $originalResponsiveImagesContent;
+    $newResponsiveImages['thumb']['base64svg'] = "data:image/svg+xml;base64,PCPg==";
+    $newResponsiveImages['thumb']['urls'][0] = $thumbResponsiveImageFileName;
+    $media->responsive_images = $newResponsiveImages;
+    $media->save();
+
+    $this->artisan('media-library:clean');
+
+    $media->refresh();
+
+    expect($media->responsive_images)->toEqual($originalResponsiveImagesContent);
+    $this->assertFileDoesNotExist($thumbReponsiveImagesPath);
 });
