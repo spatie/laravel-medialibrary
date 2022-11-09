56152a0f5fac89d3c298a0101b70f22747b64d17
rahulhaque <rahulhaque07@gmail.com>
2022-11-09 17:13:06 +0600

    Added option to set custom queue connection (#3085)

--- src/Conversions/FileManipulator.php
+++ src/Conversions/FileManipulator.php
@@ -92,6 +92,7 @@ class FileManipulator
 
         /** @var PerformConversionsJob $job */
         $job = (new $performConversionsJobClass($conversions, $media, $onlyMissing))
+            ->onConnection(config('media-library.queue_connection_name'))
             ->onQueue(config('media-library.queue_name'));
 
         dispatch($job);
@@ -116,6 +117,7 @@ class FileManipulator
 
         /** @var GenerateResponsiveImagesJob $job */
         $job = (new $generateResponsiveImagesJobClass($media))
+            ->onConnection(config('media-library.queue_connection_name'))
             ->onQueue(config('media-library.queue_name'));
 
         dispatch($job);
