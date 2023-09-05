38af83a445a9ccffede87b7251102580b6f3883f
Constantin Graf <26689068+korridor@users.noreply.github.com>
2023-09-05 09:56:04 +0200

    Use temporary folder of spatie/laravel-medialibrary for spatie/image as well (#3368)

--- src/Support/ImageFactory.php
+++ src/Support/ImageFactory.php
@@ -8,6 +8,7 @@ class ImageFactory
 {
     public static function load(string $path): Image
     {
-        return Image::load($path)->useImageDriver(config('media-library.image_driver'));
+        return Image::load($path)->useImageDriver(config('media-library.image_driver'))
+            ->setTemporaryDirectory(config('media-library.temporary_directory_path') ?? storage_path('media-library/temp'));
     }
 }
