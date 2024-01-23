12ec3fb9ba3136ff90bd469883297a5118dfbff1
jhorie <jhorie@mynober.nl>
2024-01-23 11:58:30 +0100

    Disabled tiny placeholder generation when use_tiny_placeholder config is disabled

--- config/media-library.php
+++ config/media-library.php
@@ -225,6 +225,7 @@ return [
         /*
          * By default rendering media to a responsive image will add some javascript and a tiny placeholder.
          * This ensures that the browser can already determine the correct layout.
+         * When disabled, no tiny placeholder is generated.
          */
         'use_tiny_placeholders' => true,
 
