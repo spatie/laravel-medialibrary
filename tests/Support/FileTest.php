06e8fb8d5e1d2ef9f6fa3c32a29941bfb78c9807
TheXerr0r <efma.tech@gmail.com>
2024-02-15 19:09:07 +0300

    Testing negative byte value in `getHumanReadableSize()` support function

--- tests/Support/FileTest.php
+++ tests/Support/FileTest.php
@@ -8,6 +8,7 @@ it('can determine a human readable filesize', function () {
     expect(File::getHumanReadableSize(1000))->toEqual('1000 B');
     expect(File::getHumanReadableSize(10000))->toEqual('9.77 KB');
     expect(File::getHumanReadableSize(10000))->toEqual('9.77 KB');
+    expect(File::getHumanReadableSize(-10000))->toEqual('9.77 KB');
     $this->assertEquals('976.56 KB', File::getHumanReadableSize(1_000_000));
     $this->assertEquals('9.54 MB', File::getHumanReadableSize(10_000_000));
     $this->assertEquals('9.31 GB', File::getHumanReadableSize(10_000_000_000));
@@ -16,6 +17,7 @@ it('can determine a human readable filesize', function () {
     $this->assertEquals('86.74 EB', File::getHumanReadableSize(100_000_000_000_000_000_000));
     $this->assertEquals('84.7 ZB', File::getHumanReadableSize(100_000_000_000_000_000_000_000));
     $this->assertEquals('82.72 YB', File::getHumanReadableSize(100_000_000_000_000_000_000_000_000));
+    $this->assertEquals('82.72 YB', File::getHumanReadableSize(-100_000_000_000_000_000_000_000_000));
 });
 
 it('can determine the mime type of a file', function () {
