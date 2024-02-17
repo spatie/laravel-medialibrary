20927304029f3e21874b033c77f18b3b9ab7b53a
Ehsan Mahmoodi <misaf.1990@gmail.com>
2024-02-17 15:38:02 +0330

    Update ToHtmlTest.php

--- tests/Feature/Media/ToHtmlTest.php
+++ tests/Feature/Media/ToHtmlTest.php
@@ -48,7 +48,7 @@ it('can render an array of classes as extra attributes', function () {
 
 it('can render an array of styles as extra attributes', function () {
     $this->assertEquals(
-        '<img class="rounded border" src="/media/1/conversions/test-thumb.jpg" alt="test" style="background-color: blue; color: blue;">',
+        '<img style="background-color: blue; color: blue;" class="rounded border" src="/media/1/conversions/test-thumb.jpg" alt="test">',
         Media::first()->img('thumb', ['style' => [
             'background-color: blue',
             'color: blue' => true,
