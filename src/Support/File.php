899d7f6880266f8b4bd9e3fe8107e75499fac787
ziming <ziming@users.noreply.github.com>
2024-02-20 23:05:30 +0800

    Fix DivisionByZeroError in File::getHumanReadable() (#3549)
    * Fix DivisionByZeroError
    
    * Use triple equal
    
    * Change back to double equals comparison
    
    * test for 0 bytes

--- src/Support/File.php
+++ src/Support/File.php
@@ -10,6 +10,11 @@ class File
     {
         $units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
 
+        // 0 === 0.00 return false so use double equals
+        if ($sizeInBytes == 0) {
+            return '0 '.$units[0];
+        }
+
         $index = min(count($units) - 1, floor(log(abs($sizeInBytes), 1024)));
 
         return sprintf('%s %s', round(num: abs($sizeInBytes) / (1024 ** $index), precision: 2), $units[$index]);
