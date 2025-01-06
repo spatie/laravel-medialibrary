ae4c244396ed00292fc6db0fcbb71a9ba6793ee7
Evgeniy Yurchenko <46298617+JeRabix@users.noreply.github.com>
2025-01-06 12:38:34 +0300

    chore: media relation add generic docblock (#3760)

--- src/InteractsWithMedia.php
+++ src/InteractsWithMedia.php
@@ -56,6 +56,9 @@ trait InteractsWithMedia
         });
     }
 
+    /**
+      * @return MorphMany<Media, $this>
+    */
     public function media(): MorphMany
     {
         return $this->morphMany($this->getMediaModel(), 'model');
