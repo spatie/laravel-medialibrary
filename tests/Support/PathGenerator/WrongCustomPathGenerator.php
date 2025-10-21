cb32ce72c1b3e6592ffc0003b2c15ed7101dad76
freekmurze <483853+freekmurze@users.noreply.github.com>
2025-10-21 06:59:30 +0000

    Fix styling

--- tests/Support/PathGenerator/WrongCustomPathGenerator.php
+++ tests/Support/PathGenerator/WrongCustomPathGenerator.php
@@ -2,10 +2,4 @@
 
 namespace Spatie\MediaLibrary\Tests\Support\PathGenerator;
 
-use Spatie\MediaLibrary\MediaCollections\Models\Media;
-use Spatie\MediaLibrary\Support\PathGenerator\PathGenerator;
-
-class WrongCustomPathGenerator
-{
-
-}
+class WrongCustomPathGenerator {}
