b11bb41556be65671b56d0b5755b966b3b047fb0
Freek Van der Herten <freek@spatie.be>
2026-06-15 10:35:14 +0200

    Add MediaCollection attribute

---
+++ src/Attributes/MediaCollection.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Spatie\MediaLibrary\Attributes;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_CLASS | Attribute::IS_REPEATABLE)]
+class MediaCollection
+{
+    public function __construct(
+        public string $name,
+        public bool $singleFile = false,
+        public ?int $onlyKeepLatest = null,
+        public array $acceptsMimeTypes = [],
+        public ?string $disk = null,
+        public ?string $conversionsDisk = null,
+        public ?string $fallbackUrl = null,
+        public ?string $fallbackPath = null,
+        public bool $responsiveImages = false,
+    ) {}
+}
