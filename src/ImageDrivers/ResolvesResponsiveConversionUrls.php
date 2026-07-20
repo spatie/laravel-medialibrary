7392514f0d2d9f2f53408905fb694f75267cdc0c
freekmurze <483853+freekmurze@users.noreply.github.com>
2026-07-20 21:27:07 +0000

    Fix styling

--- src/ImageDrivers/ResolvesResponsiveConversionUrls.php
+++ src/ImageDrivers/ResolvesResponsiveConversionUrls.php
@@ -11,7 +11,7 @@ interface ResolvesResponsiveConversionUrls extends ResolvesConversionUrls
      * Build a set of urls for a virtual conversion at different widths, used to
      * emit a responsive srcset without generating or storing any files.
      *
-     * @return array<int, string>  Keyed by width, e.g. [320 => 'https://...', 640 => '...'].
+     * @return array<int, string> Keyed by width, e.g. [320 => 'https://...', 640 => '...'].
      */
     public function responsiveConversionUrls(Media $media, Conversion $conversion): array;
 }
